52fce8942dff6a2e2990de870bc6e776d6ee6e88
nhi nguyen 8888 <nhinguyen@nhis-MacBook-Pro.local>
2020-05-23 16:22:47 -0400

    finish checking for dublicate item when add

--- redux/reducer.js
+++ redux/reducer.js
@@ -6,7 +6,12 @@ const initialState = {
 export default function(state=initialState, action) {
     switch(action.type){
         case 'ADD_TO_CART':
+            // check for dublicate item
             const item = action.payload
+            const existsInArray = state.cart.some(l => l.id === item.id)
+            if(existsInArray) {
+                return state;
+              }
             return {
                 ...state,
                 cart: [item, ...state.cart]
